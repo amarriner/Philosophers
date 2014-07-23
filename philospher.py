15f6ec3ab6f5eff0bcbb05d7ba518a0af96fdd80
Aaron Marriner <amarriner@amarriner.com>
2014-07-23 14:58:30 +0000

    Initial commit

---
+++ philospher.py
@@ -0,0 +1,14 @@
+#!/home/amarriner/.virtualenvs/philosopher/bin/python
+
+"""Script to parse quotes from wikiquotes and do silly things to them"""
+import json
+import os
+import requests
+import sys
+
+def main():
+   """Main entry point"""
+   pass
+
+if __name__ == '__main__':
+   sys.exit(main())
